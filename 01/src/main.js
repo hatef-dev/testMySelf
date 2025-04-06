4e799e0321fcb181fb4afffa5ea6cc8643708da2
unknown <hatef.dev21@gmail.com>
2025-04-06 13:31:03 +0330

    add particles

--- 01/src/main.js
+++ 01/src/main.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 
 const gui = new GUI();
 const parameters = {
-  count: 500,
+  count: 1500,
 };
 
 const canvas = document.querySelector("#webgl");
@@ -28,12 +28,11 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 const textureLoader = new THREE.TextureLoader();
-const color = textureLoader.load(".");
+const color = textureLoader.load("./textures/particles/2.png");
 
 let particleGeometry = null;
 let particleMaterial = null;
 let particleSystem = null;
-
 
 const generateParticles = () => {
   if (particleSystem !== null) {
@@ -41,36 +40,44 @@ const generateParticles = () => {
     particleMaterial.dispose();
     scene.remove(particleSystem);
   }
-  
+
   const positions = new Float32Array(parameters.count * 3);
-  
+  const colors = new Float32Array(parameters.count * 3);
   for (let i = 0; i < parameters.count; i++) {
-      positions[i] = (Math.random() - 0.5) * 5;
-    }
-    
-    particleGeometry = new THREE.BufferGeometry();
-    particleGeometry.setAttribute(
-        "position",
-        new THREE.BufferAttribute(positions, 3)
-    );
-    
-    particleMaterial = new THREE.PointsMaterial({
-        size: 0.03,
-        sizeAttenuation: true,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending,
-        color: "white",
-    });
-    
-    particleSystem = new THREE.Points(particleGeometry, particleMaterial);
-    scene.add(particleSystem);
+    positions[i * 3] = (Math.random() - 0.5) * 3; // x
+    positions[i * 3 + 1] = (Math.random() - 0.5) * 3; // y
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 3; // z
+    colors[i * 3] = Math.random();
+    colors[i * 3 + 1] = Math.random();
+    colors[i * 3 + 2] = Math.random();
+  }
+
+  particleGeometry = new THREE.BufferGeometry();
+  particleGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3)
+  );
+  particleGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+  particleMaterial = new THREE.PointsMaterial({
+    size: 0.05,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+
+    vertexColors: true,
+    transparent: true,
+    alphaMap: color,
+  });
+
+  particleSystem = new THREE.Points(particleGeometry, particleMaterial);
+  scene.add(particleSystem);
 };
 generateParticles();
 
 gui
   .add(parameters, "count")
   .min(100)
-  .max(10000)
+  .max(100000)
   .step(100)
   .onFinishChange(generateParticles);
 
@@ -92,6 +99,12 @@ const clock = new THREE.Clock();
 
 const animate = () => {
   const elapsedTime = clock.getElapsedTime();
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+    const x = particleGeometry.attributes.position.array[i3];
+    particleGeometry.attributes.position.array[i3 + 1] = Math.sin(x * elapsedTime);
+  }
+  particleGeometry.attributes.position.needsUpdate = true;
   window.requestAnimationFrame(animate);
   renderer.render(scene, camera);
   controls.update();
