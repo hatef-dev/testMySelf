242279ee475e582a490809186557b8e36d86a2ed
unknown <hatef.dev21@gmail.com>
2025-04-06 14:08:19 +0330

    add particles and lil-gui

--- 03/src/main.js
+++ 03/src/main.js
@@ -1,7 +1,77 @@
 import * as THREE from "three";
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
 const scene = new THREE.Scene();
+const gui = new GUI();
+const canvas = document.querySelector("#webgl");
+
+const parameters = {
+  count: 600,
+  spin: 1,
+  branches: 3,
+
+};
+
+
+let particlesGeometry = null;
+let particlesMaterial = null;
+let particles = null;
+
+const generateParticles = () => {
+  if (particles !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(particles);
+  }
+
+  particlesGeometry = new THREE.BufferGeometry();
+  const positions = new Float32Array(parameters.count * 3);
+  for (let i = 0; i < parameters.count; i++) {
+    positions[i * 3] = (Math.random() - 0.5) * 3;
+    positions[i * 3 + 1] = 0;
+    positions[i * 3 + 2] = 0;
+  }
+  particlesGeometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+  particlesMaterial = new THREE.PointsMaterial({
+    size: 0.03,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    color: "#ffffff",
+  });
+  particles = new THREE.Points(particlesGeometry, particlesMaterial);
+  scene.add(particles);
+}
+gui
+generateParticles();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 camera.position.z = 5;
-scene.add(camera);
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
+});
+
+
+const clock = new THREE.Clock();
+
+const animate = () => {
+  window.requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+};
+animate()
